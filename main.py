4a0011e459f42ed3cc5d064dceab8cfbc1de123a
JCionx <92257741+JCionx@users.noreply.github.com>
2024-08-01 21:51:09 +0100

    added german burger king support

--- main.py
+++ main.py
@@ -40,15 +40,17 @@ app.add_middleware(AuthMiddleware)
 @ui.page('/')
 def main_page() -> None:
     steps = 24
-    async def run(playwright: Playwright):
+    steps_de = 23
+
+    async def run_pt(playwright: Playwright):
         ui.notify("Generating code...")
         progressbar.set_visibility(True)
-        generate_button.set_visibility(False)
+        generate_buttons.set_visibility(False)
 
         chromium = playwright.chromium
         browser = await chromium.launch(headless=True)
         page = await browser.new_page()
-        await page.goto("https://rbixm.qualtrics.com/jfe/form/SV_9MHgHFvPm0OEHr0?CountryCode=PRT&Q_Language=PT&PT=1")  # Replace with the actual URL
+        await page.goto("https://rbixm.qualtrics.com/jfe/form/SV_9MHgHFvPm0OEHr0?CountryCode=DEU&Q_Language=DE&PT=1")  # Replace with the actual URL
 
         # Burger King number
         progressbar.value = 1 / steps
@@ -193,7 +195,7 @@ def main_page() -> None:
         # Show the validation code
         progressbar.set_visibility(False)
         progressbar.value = 0
-        generate_button.set_visibility(True)
+        generate_buttons.set_visibility(True)
 
         result = await (await page.wait_for_selector("#EndOfSurvey")).inner_text()
         survey_code = result.split("Código de validação: ")[1][:7]
@@ -203,9 +205,171 @@ def main_page() -> None:
 
         await browser.close()
 
-    async def main():
+    async def run_de(playwright: Playwright):
+        ui.notify("Generating code...")
+        progressbar.set_visibility(True)
+        generate_buttons.set_visibility(False)
+
+        chromium = playwright.chromium
+        browser = await chromium.launch(headless=True)
+        page = await browser.new_page()
+        await page.goto("https://rbixm.qualtrics.com/jfe/form/SV_9MHgHFvPm0OEHr0?CountryCode=DEU&Q_Language=DE&PT=1")  # Replace with the actual URL
+
+        # Burger King number
+        progressbar.value = 1 / steps_de
+        await (await page.wait_for_selector(".QR-QID4")).fill("15652")
+        await asyncio.sleep(1.5)
+        await (await page.wait_for_selector("#NextButton")).click()
+
+        # Date and time of visit
+        progressbar.value = 2 / steps_de
+        await (await page.wait_for_selector(".QR-QID118-2")).click()
+        await asyncio.sleep(0.1)
+        await (await page.wait_for_selector(".ui-datepicker-today")).click()
+        await (await page.wait_for_selector('[name="QR~QID8#1~1"]')).select_option("8")
+        await (await page.wait_for_selector('[name="QR~QID8#2~1"]')).select_option("4")
+        await (await page.wait_for_selector('[name="QR~QID8#3~1"]')).select_option("1")
+        await asyncio.sleep(0.4)
+        await (await page.wait_for_selector("#NextButton")).click()
+
+        # Order type
+        progressbar.value = 3 / steps_de
+        await (await page.wait_for_selector("#QID12-1-label")).click()
+        await (await page.wait_for_selector("#NextButton")).click()
+
+        # Order receiving method
+        progressbar.value = 4 / steps_de
+        await (await page.wait_for_selector("#QID14-2-label")).click()
+        await (await page.wait_for_selector("#NextButton")).click()
+
+        # How satisfied were you with the visit
+        progressbar.value = 5 / steps_de
+        await (await page.wait_for_selector("#QID18-17-label")).click()
+        await (await page.wait_for_selector("#NextButton")).click()
+
+        # Tell us in three sentences what you liked the most about your visit
+        progressbar.value = 6 / steps_de
+        await (await page.wait_for_selector(".QR-QID117")).fill("...")
+        await (await page.wait_for_selector("#NextButton")).click()
+
+        # Classify your satisfaction with the service
+        progressbar.value = 7 / steps_de
+        await (await page.wait_for_selector('[for="QR~QID121~4~12"]')).click()
+        await (await page.wait_for_selector('[for="QR~QID121~5~12"]')).click()
+        await (await page.wait_for_selector('[for="QR~QID121~10~12"]')).click()
+        await (await page.wait_for_selector('[for="QR~QID121~11~12"]')).click()
+        await (await page.wait_for_selector('[for="QR~QID121~13~12"]')).click()
+        await (await page.wait_for_selector('[for="QR~QID121~16~12"]')).click()
+        await (await page.wait_for_selector("#NextButton")).click()
+
+        # Classify the restaurant hygiene
+        progressbar.value = 8 / steps_de
+        await (await page.wait_for_selector('[for="QR~QID123~7~12"]')).click()
+        await (await page.wait_for_selector('[for="QR~QID123~9~12"]')).click()
+        await (await page.wait_for_selector("#NextButton")).click()
+
+        # Classify the satisfaction with the advanced hygiene measures
+        progressbar.value = 9 / steps_de
+        await (await page.wait_for_selector('#QID122-8-label')).click()
+        await (await page.wait_for_selector("#NextButton")).click()
+
+        # Classify the satisfaction with the food
+        progressbar.value = 10 / steps_de
+        await (await page.wait_for_selector('[for="QR~QID21~1~12"]')).click()
+        await (await page.wait_for_selector('[for="QR~QID21~2~12"]')).click()
+        await (await page.wait_for_selector('[for="QR~QID21~3~12"]')).click()
+        await (await page.wait_for_selector('[for="QR~QID21~20~12"]')).click()
+        await (await page.wait_for_selector("#NextButton")).click()
+
+        # Classify the satisfaction with the item
+        progressbar.value = 11 / steps_de
+        await (await page.wait_for_selector('[for="QR~QID22~1~8"]')).click()
+        await (await page.wait_for_selector('[for="QR~QID22~2~13"]')).click()
+        await (await page.wait_for_selector('[for="QR~QID22~3~13"]')).click()
+        await (await page.wait_for_selector('[for="QR~QID22~4~13"]')).click()
+        await (await page.wait_for_selector("#NextButton")).click()
+
+        # Did any problem occur during the visit?
+        progressbar.value = 12 / steps_de
+        await (await page.wait_for_selector("#QID38-2-label")).click()
+        await (await page.wait_for_selector("#NextButton")).click()
+
+        # Based on your experience, how likely are you to recommend Burger King to a friend or family member?
+        progressbar.value = 13 / steps_de
+        await (await page.wait_for_selector('[for="QR~QID41~1~6"]')).click()
+        await (await page.wait_for_selector('[for="QR~QID41~2~6"]')).click()
+        await (await page.wait_for_selector("#NextButton")).click()
+
+        # What main item did you order?
+        progressbar.value = 14 / steps_de
+        await (await page.wait_for_selector('#QID46-312-label')).click()
+        await (await page.wait_for_selector("#NextButton")).click()
+
+        # What extras or desserts did you order?
+        progressbar.value = 15 / steps_de
+        await (await page.wait_for_selector('#QID47-128-label')).click()
+        await (await page.wait_for_selector("#NextButton")).click()
+
+        # How many people were in your group?
+        progressbar.value = 16 / steps_de
+        await (await page.wait_for_selector('#QID55-1-label')).click()
+        await (await page.wait_for_selector("#NextButton")).click()
+
+        # Did you ask for a custom order?
+        progressbar.value = 17 / steps_de
+        await (await page.wait_for_selector("#QID57-2-label")).click()
+        await (await page.wait_for_selector("#NextButton")).click()
+
+        # How many times have you visited a Burger King in the last month?
+        progressbar.value = 18 / steps_de
+        await (await page.wait_for_selector("#QID61-1-label")).click()
+        await (await page.wait_for_selector("#NextButton")).click()
+
+        # Why did you visit Burger King today?
+        progressbar.value = 19 / steps_de
+        await (await page.wait_for_selector("#QID62-3-label")).click()
+        await (await page.wait_for_selector("#NextButton")).click()
+
+        # What restaurant do you visit most often?
+        progressbar.value = 20 / steps_de
+        await (await page.wait_for_selector("#QID111-1-label")).click()
+        await (await page.wait_for_selector("#NextButton")).click()
+
+        # Would you like to shout out to any employee?
+        progressbar.value = 21 / steps_de
+        await (await page.wait_for_selector("#QID78-2-label")).click()
+        await (await page.wait_for_selector("#NextButton")).click()
+
+        # Final questions
+        progressbar.value = 22 / steps_de
+        await asyncio.sleep(1)
+        await (await page.wait_for_selector("#NextButton")).click()
+
+        # What's your household income?
+        progressbar.value = 23 / steps_de
+        await (await page.wait_for_selector("#QID67-9-label")).click()
+        await (await page.wait_for_selector("#NextButton")).click()
+
+        # Show the validation code
+        progressbar.set_visibility(False)
+        progressbar.value = 0
+        generate_buttons.set_visibility(True)
+
+        result = await (await page.wait_for_selector("#EndOfSurvey")).inner_text()
+        survey_code = result.split("Validierungscode: ")[1][:7]
+        ui.notify("Completed")
+        code_dialog.open()
+        code_label.set_text(survey_code)
+
+        await browser.close()
+
+    async def main_pt():
         async with async_playwright() as playwright:
-            await run(playwright)
+            await run_pt(playwright)
+
+    async def main_de():
+        async with async_playwright() as playwright:
+            await run_de(playwright)
 
     ui.page_title("BK Skip")
 
@@ -213,7 +377,10 @@ def main_page() -> None:
         ui.label('BK Skip').style("font-size: 180%; width: 100%; text-align: center;")
         progressbar = ui.linear_progress(value=0, show_value=False)
         progressbar.set_visibility(False)
-        generate_button = ui.button('Generate code', on_click=lambda: main())
+
+        with ui.column() as generate_buttons:
+            ui.button('Generate code 🇵🇹', on_click=lambda: main_pt())
+            ui.button('Generate code 🇩🇪', on_click=lambda: main_de())
 
         with ui.dialog() as code_dialog, ui.card():
             ui.label('The code is:').style("margin: 0 80px; text-align: center;")
